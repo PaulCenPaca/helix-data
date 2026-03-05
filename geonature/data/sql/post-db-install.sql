896361fb7b6147efa2321c318f602ecc7f81f80f
Paul Honore <paul.honore@cen-paca.org>
2026-03-05 12:02:39 +0100

    feat: script sql initialisation

--- geonature/data/sql/post-db-install.sql
+++ geonature/data/sql/post-db-install.sql
@@ -1,16 +1,5 @@
 BEGIN;
 
-TRUNCATE TABLE pr_occtax.t_releves_occtax CASCADE;
-ALTER SEQUENCE pr_occtax.t_releves_occtax_id_releve_occtax_seq RESTART WITH 1;
-ALTER SEQUENCE pr_occtax.t_occurrences_occtax_id_occurrence_occtax_seq RESTART WITH 1;
-ALTER SEQUENCE pr_occtax.cor_counting_occtax_id_counting_occtax_seq RESTART WITH 1;
---delete from gn_permissions.t_permissions where created_on>'2025-10-10 18:00:00';
-TRUNCATE TABLE gn_synthese.synthese CASCADE;
-ALTER SEQUENCE gn_synthese.synthese_id_synthese_seq RESTART WITH 1;
-TRUNCATE TABLE gn_commons.t_validations CASCADE;
-ALTER SEQUENCE gn_commons.t_validations_id_validation_seq RESTART WITH 1;
-
-
 -- \echo '-------------------------------------------------------------------------------'
 -- \echo 'Finalize integration of GN database'
 
